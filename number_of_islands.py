a3f74b3a1af5803c709a7308433c660181b46a04
Rashi Bansal <rashibansal79518@gmail.com>
2020-06-29 09:36:25 +0530

    Numbers of Islands
    https://leetcode.com/problems/number-of-islands/

---
+++ number_of_islands.py
@@ -0,0 +1,43 @@
+'''Number Of Islands'''
+
+#https://leetcode.com/problems/number-of-islands/
+'''Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+Example 2:
+
+Input:
+11000
+11000
+00100
+00011
+
+Output: 3;'''
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        number_of_islands=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]=='1':
+                    self.dfs(grid,i,j)
+                    number_of_islands+=1
+        return number_of_islands
+    
+    def dfs(self,grid,i,j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]!='1' :
+            return 0
+        grid[i][j]='0' #visited
+        #Look for island in every direction
+        self.dfs(grid,i-1,j)
+        self.dfs(grid,i+1,j)
+        self.dfs(grid,i,j-1)
+        self.dfs(grid,i,j+1)
