8db7d18f1724ebe78496ba1e4ced9922e2a94fa4
Rashi Bansal <rashibansal79518@gmail.com>
2020-07-13 11:52:11 +0530

    Group anagrams
    https://leetcode.com/problems/group-anagrams/

---
+++ Group-Anagrams.py
@@ -0,0 +1,33 @@
+#https://leetcode.com/problems/group-anagrams/
+'''
+49. Group Anagrams
+
+Given an array of strings, group anagrams together.
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
+
+'''
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        h = {}
+        for word in strs:
+            sortedWord = ''.join(sorted(word))
+            if sortedWord not in h:
+                h[sortedWord] = [word]
+            else:
+                h[sortedWord].append(word)
+        final = []
+        for value in h.values():
+            final.append(value)
+        return final
