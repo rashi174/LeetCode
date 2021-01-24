3fdf28704f72f5e3de4abad0a73edb52c3fb4015
Rashi Bansal <rashibansal79518@gmail.com>
2021-01-24 19:39:32 +0530

    Fizz Buzz
    https://leetcode.com/problems/fizz-buzz/solution/

---
+++ FizzBuzz.java
@@ -0,0 +1,128 @@
+/*
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+Example:
+
+n = 15,
+
+Return:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+]
+
+
+
+
+You must have played FizzBuzz as kids. FizzBuzz charm never gets old. And so here we are looking at how you can take on one step at a time and impress your interviewer with a better and neat approach to solve this problem.
+
+Approach 1: Naive Approach
+Intuition
+
+The moment you hear of FizzBuzz you think whether the number is divisible by 3, 5 or both.
+
+Algorithm
+
+1.Initialize an empty answer list.
+2.Iterate on the numbers from 1 ... N1...N.
+3.For every number, if it is divisible by both 3 and 5, add FizzBuzz to the answer list.
+4.Else, Check if the number is divisible by 3, add Fizz.
+5.Else, Check if the number is divisible by 5, add Buzz.
+6.Else, add the number.
+
+*/
+
+
+class Solution {
+  public List<String> fizzBuzz(int n) {
+
+    // ans list
+    List<String> ans = new ArrayList<String>();
+
+    for (int num = 1; num <= n; num++) {
+
+      boolean divisibleBy3 = (num % 3 == 0);
+      boolean divisibleBy5 = (num % 5 == 0);
+
+      if (divisibleBy3 && divisibleBy5) {
+        // Divides by both 3 and 5, add FizzBuzz
+        ans.add("FizzBuzz");
+      } else if (divisibleBy3) {
+        // Divides by 3, add Fizz
+        ans.add("Fizz");
+      } else if (divisibleBy5) {
+        // Divides by 5, add Buzz
+        ans.add("Buzz");
+      } else {
+        // Not divisible by 3 or 5, add the number
+        ans.add(Integer.toString(num));
+      }
+    }
+
+    return ans;
+  }
+}
+
+//Complexity Analysis
+
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+
+
+
+
+class Solution {
+  public List<String> fizzBuzz(int n) {
+
+    // ans list
+    List<String> ans = new ArrayList<String>();
+
+    // Hash map to store all fizzbuzz mappings.
+    HashMap<Integer, String> fizzBizzDict =
+        new HashMap<Integer, String>() {
+          {
+            put(3, "Fizz");
+            put(5, "Buzz");
+          }
+        };
+
+    for (int num = 1; num <= n; num++) {
+
+      String numAnsStr = "";
+
+      for (Integer key : fizzBizzDict.keySet()) {
+
+        // If the num is divisible by key,
+        // then add the corresponding string mapping to current numAnsStr
+        if (num % key == 0) {
+          numAnsStr += fizzBizzDict.get(key);
+        }
+      }
+
+      if (numAnsStr.equals("")) {
+        // Not divisible by 3 or 5, add the number
+        numAnsStr += Integer.toString(num);
+      }
+
+      // Append the current answer str to the ans list
+      ans.add(numAnsStr);
+    }
+
+    return ans;
+  }
+}
